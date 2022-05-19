78d6de2248bbcf50afc1c828de3dd5a8b2997150
ShamaySapir <shamay.sapir@gmail.com>
2022-05-19 09:24:45 +0300

    change js to ts

--- pages/api/examples/session.ts
+++
@@ -1,9 +0,0 @@
-// This is an example of how to access a session from an API route
-import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-  res.send(JSON.stringify(session, null, 2));
-};
-export default handler;
--- pages/api/test.ts
+++
@@ -1,7 +0,0 @@
-// This is an example of how to read a JSON Web Token from an API route
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.send(JSON.stringify({ test: 123 }));
-};
-export default handler;
